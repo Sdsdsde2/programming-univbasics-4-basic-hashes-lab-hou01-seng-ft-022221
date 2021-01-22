c0c6705392664a6dadf5f5653f0e2944595b3258
Shane R <imspammails@gmail.com>
2021-01-22 17:08:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,20 @@ def new_hash
 end
 
 def my_hash
-  my_hash 
+  my_hash = {
+    my_key: my_value
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer_hash = {
+    :name => 'Grace Hopper'
+  }
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  generated_hash = {
+    :id => number
+  }
 end
